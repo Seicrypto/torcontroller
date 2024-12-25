9142145149e64b6d954f9a5855acc0f814bc4d1a
Sei <97655693+Seicrypto@users.noreply.github.com>
2024-12-25 12:54:42 +0800

    Added systemd debian to test env Chore: - Added systemd install. Fix: - Fixed version cmd format.

--- cmd/version.go
+++ cmd/version.go
@@ -7,11 +7,10 @@ import (
 )
 
 var VersionCmd = &cobra.Command{
-	Use:     "version",
-	Aliases: []string{"--version", "-v"},
-	Short:   "Show TorController version",
-	Long:    "TorController CLI version 1.1.0\n\nVisit https://github.com/Seicrypto/torcontroller for release notes and updates.",
-	Args:    cobra.NoArgs,
+	Use:   "version",
+	Short: "Show TorController version",
+	Long:  "TorController CLI version 1.1.0\n\nVisit https://github.com/Seicrypto/torcontroller for release notes and updates.",
+	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("TorController version 1.1.0")
 	},
